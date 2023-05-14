b314c17ca9e98287e853103c475b15ca18e7fc57
vishnupriyan <priyanvishnu15@gmail.com>
2023-05-14 13:00:13 +0530

    initial commit

---
+++ account/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from account.views import SendPasswordResetEmailView, UserChangePasswordView, UserLoginView, UserProfileView, UserRegistrationView, UserPasswordResetView
+from . import views
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
+    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
+    path('enquiry-list/', views.enquiryList, name='enquiry-list'),
+    path('enquiry-detail/<str:pk>/', views.enquiryDetail, name='enquiry-detail'),
+    path('enquiry-update/<str:pk>/', views.enquiryUpdate, name='enquiry-update'),
+    path('enquiry-delete/<str:pk>/', views.enquiryDelete, name='enquiry-delete'),
+    path('enquiry-create/', views.enquiryCreate, name='enquiry-create'),
+    path('endoresment-create/', views.endoresmentCreate, name='endoresment-create'),
+    path('endoresment-list/', views.endoresmentList, name='endoresment-list'),
+    path('endoresment-detail/<str:pk>/', views.endoresmentDetail, name='endoresment-detail'),
+    path('endoresment-update/<str:pk>/', views.endoresmentUpdate, name='endoresment-update'),
+    path('endoresment-delete/<str:pk>/', views.endoresmentDelete, name='endoresment-delete'),
+    path('check-user/', views.isUserAdmin, name='check-user'),
+]
---
+++ account/views.py
@@ -0,0 +1,180 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
+from account.models import Endoresment, Enquiry
+from account.serializers import (
+    EndoresmentSerializer,
+    SendPasswordResetEmailSerializer,
+    UserChangePasswordSerializer,
+    UserLoginSerializer,
+    UserPasswordResetSerializer,
+    UserProfileSerializer,
+    UserRegistrationSerializer,
+    EnquirySerializer
+)
+from django.contrib.auth import authenticate
+from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+
+# Generate Token Manually
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+    
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def isUserAdmin(request):
+    content = {
+        'msg': True
+    }
+    return Response(content)
+
+class UserRegistrationView(APIView):
+    renderer_classes = [UserRenderer]
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = get_tokens_for_user(user)
+        return Response({'token':token, 'msg':'Registration Successful'}, status=status.HTTP_201_CREATED)
+
+class UserLoginView(APIView):
+    renderer_classes = [UserRenderer]
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.data.get('email')
+        password = serializer.data.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            token = get_tokens_for_user(user)
+            return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+
+class UserProfileView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserChangePasswordView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
+
+class SendPasswordResetEmailView(APIView):
+    renderer_classes = [UserRenderer]
+    def post(self, request, format=None):
+        serializer = SendPasswordResetEmailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Password Reset link send. Please check your Email'}, status=status.HTTP_200_OK)
+
+class UserPasswordResetView(APIView):
+    renderer_classes = [UserRenderer]
+    def post(self, request, uid, token, format=None):
+        serializer = UserPasswordResetSerializer(data=request.data, context={'uid':uid, 'token':token})
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Password Reset Successfully'}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def enquiryList(request):
+    enquires = Enquiry.objects.all()
+    serializer = EnquirySerializer(enquires, many=True)
+    if request.user.is_authenticated and request.user.is_admin:
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response({'msg':'You are Not Authorised to view this data'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+@api_view(['GET'])
+def enquiryDetail(request, pk):
+    enquiry = Enquiry.objects.get(id=pk)
+    serializer = EnquirySerializer(enquiry, many=False)
+    if request.user.is_authenticated and request.user.is_admin:
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response({'msg':'You are Not Authorised to view this data'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+@api_view(['POST'])
+def enquiryCreate(request):
+    serializer = EnquirySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def enquiryUpdate(request, pk):
+    isAdmin = isUserAdmin
+    if isAdmin:
+        enquiry = Enquiry.objects.get(id=pk)
+        serializer = EnquirySerializer(instance=enquiry, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response({'msg':'You are Not Authorised to view this data'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['DELETE'])
+def enquiryDelete(request, pk):
+    isAdmin = isUserAdmin
+    if isAdmin:
+        enquiry = Enquiry.objects.get(id=pk)
+        enquiry.delete()
+        return Response({'msg': 'Deleted Successfully'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'msg': 'You are Not Admin user to Access the data'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+@api_view(['GET'])
+def endoresmentList(request):
+    isAdmin = isUserAdmin
+    if isAdmin:
+        endoresment = Endoresment.objects.all()
+        serializer = EndoresmentSerializer(endoresment, many=True, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+@api_view(['GET'])
+def endoresmentDetail(request, pk):
+    endoresment = Endoresment.objects.get(id=pk)
+    serializer = EndoresmentSerializer(endoresment, many=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def endoresmentCreate(request):
+    isAdmin = isUserAdmin
+    if isAdmin:
+        serializer = EndoresmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response({'msg':'You are Not Authorised to view this data'}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['POST'])
+def endoresmentUpdate(request, pk):
+    isAdmin = isUserAdmin
+    if isAdmin:
+        endoresment = Endoresment.objects.get(id=pk)
+        serializer = EndoresmentSerializer(instance=endoresment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response({'msg':'You are Not Authorised to view this data'}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['DELETE'])
+def endoresmentDelete(request, pk):
+    isAdmin = isUserAdmin
+    if isAdmin:
+        endoresment = Endoresment.objects.get(id=pk)
+        endoresment.delete()
+        return Response({'msg': 'Deleted Successfully'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'msg': 'You are Not Admin user to Access the data'}, status=status.HTTP_401_UNAUTHORIZED)
